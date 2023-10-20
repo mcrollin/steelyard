f973a8498c3810f8595447e7f5c19401526de643
Marc Rollin <rollin.marc@gmail.com>
2023-10-20 19:18:26 +0200

    fine tune the main command's abstract

--- Sources/Steelyard/SteelyardCommand.swift
+++ Sources/Steelyard/SteelyardCommand.swift
@@ -11,7 +11,7 @@ struct SteelyardCommand: AsyncParsableCommand {
 
     static var configuration = CommandConfiguration(
         commandName: "steelyard",
-        abstract: "Analytics and visualization app size utility for Apple platforms developers.",
+        abstract: "Generate insightful App Size Graphs & JSON Metrics using App Store Connect API.",
         subcommands: [GraphCommand.self, DataCommand.self]
     )
 }
